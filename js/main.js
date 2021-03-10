3baacf67cb75018092f19a0ace9429d02445527a
Gustavo Fonseca <52971006+Gugotze@users.noreply.github.com>
2021-03-10 01:41:31 -0300

    Implementação Produto
    Inicio da implementação da apresentação da apresentação do produto especifico

--- js/main.js
+++ js/main.js
@@ -14,12 +14,32 @@ function getProdutos() {
             var produtos = data._produto;
             console.log(tamanho);
             console.log(produtos);
-            for (i = 0; i < 4; i++) {
+            for (i = 0; i < tamanho; i++) {
                 var produto = produtos[i];
 
                 retornarDiv(produto);
 
             }
+        },
+        error: result => {
+            alert(result.status + ' ' + result.statusText);
+        }
+    });
+}
+
+function getProdutoEspecifico() {
+
+    var url = urlProdutoPorID();
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        timeout: 20000,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: data => {
+            console.log(data);
+            var produto = data._produto;
         },
         error: result => {
             alert(result.status + ' ' + result.statusText);
@@ -66,6 +86,15 @@ function tratarDadosgetProdutos() {
     return url1;
 }
 
+function urlProdutoPorID() {
+
+    var url = 'http://localhost:8080/Produtos?Id=';
+    var queryString = window.location.search;
+    var id = queryString.replace(/[^0-9]/g, '');
+    return url + id;
+
+}
+
 function mostrarEstrelas(qtdEstrela) {
 
     let fullStar = '<i class="fas fa-star"></i>';
@@ -89,8 +118,8 @@ function retornarDiv(response) {
     console.log(response)
     response._preco = response._preco.toFixed(2);
     return $("#divPrincipal").append(
-        '<a href="' + 'produto-especifico.html' + '" class="produtos-container col-md-3">' +
-        '<img src="'+response._imagem+'" class="img-fluid" alt="Cropped rosa">' +
+        '<a href="' + 'produto-especifico.html?Id=' + response._idProduto + '" class="produtos-container col-md-3">' +
+        '<img src="' + response._imagem + '"class="img-fluid" alt=' + response._nomeProduto + '>' +
         '<article class="produtos-itens">' +
         '<h2>' + response._nomeProduto + '</h2>' +
         '<div class="produtos-stars">' + mostrarEstrelas(response._qualidadeProduto) + '</div>' +
