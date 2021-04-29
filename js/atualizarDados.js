bc83c99b5980b01c1efb6311c6ac19a39842caf7
joaovitor2019 <55900057+joaovitor2019@users.noreply.github.com>
2021-04-29 16:00:05 -0300

    listar enderecos

--- js/atualizarDados.js
+++ js/atualizarDados.js
@@ -1,6 +1,6 @@
-$(document).ready(function(e){
+$(document).ready(function(e) {
 
-   
+
 
     $("#updateEndereco").hide();
 
@@ -16,50 +16,50 @@ $(document).ready(function(e){
     });
 
 
-    $('#Endereco').click(function(e){
+    $('#Endereco').click(function(e) {
         e.preventDefault();
         $("#Cadastro").attr("disabled", true);
-        $( "#updateDados" ).fadeOut( 500, function() {
-            $( "#updateEndereco" ).fadeIn( 500, function(){
+        $("#updateDados").fadeOut(500, function() {
+            $("#updateEndereco").fadeIn(500, function() {
                 $("#Cadastro").attr("disabled", false);
-            } );
-            
-          });
+            });
 
-          
+        });
+
+
     });
 
-    $('#Cadastro').click(function(e){
+    $('#Cadastro').click(function(e) {
         e.preventDefault();
         $("#Endereco").attr("disabled", true);
-        $( "#updateEndereco" ).fadeOut( 500, function() {
-            $( "#updateDados" ).fadeIn( 500, function(){
+        $("#updateEndereco").fadeOut(500, function() {
+            $("#updateDados").fadeIn(500, function() {
                 $("#Endereco").attr("disabled", false);
-            } );
-          });
+            });
+        });
 
-          
+
     });
 
 
-  /****************************************************************/
+    /****************************************************************/
 
     /****************************************************************/
 
 
 
 
-   
 
 
 
-    $('#enderecoFatura').click(function (e){  
+
+    $('#enderecoFatura').click(function(e) {
         var status = document.getElementById('enderecoFatura');
 
 
-        if(!status.checked){
+        if (!status.checked) {
             $('#enderecoFaturaDiv').show(1000);
-        }else{
+        } else {
             $('#enderecoFaturaDiv').hide(1000);
         }
     });
@@ -68,56 +68,57 @@ $(document).ready(function(e){
 });
 
 
-function buscarCliente(){
+function buscarCliente() {
     var token = localStorage.getItem('token');
 
     var logado = verificaSeLogado(token);
 
-    if(logado){
-    var url =  urlClientePorID();
-            $.ajax({
-                url: url,
-                type: 'GET',
-                headers: {'TOKEN': token},
-                timeout: 20000,
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: data => {
-                    console.log(data);
-                    var cliente = data;
-                    poupularCadastro(cliente)
-                },
-                error: result => {
-                    alert(result.status + ' ' + result.statusText);
-                }
-            });
-    }else{
+    if (logado) {
+        var url = urlClientePorID();
+        $.ajax({
+            url: url,
+            type: 'GET',
+            headers: { 'TOKEN': token },
+            timeout: 20000,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: data => {
+                console.log(data);
+                var cliente = data;
+                console.log(data);
+                poupularCadastro(cliente)
+            },
+            error: result => {
+                alert(result.status + ' ' + result.statusText);
+            }
+        });
+    } else {
         alert('Erro-->> usuário não logado ou sem permissão')
     }
-  }
+}
 
 
 
-  function verificaSeLogado(token){
+function verificaSeLogado(token) {
     var dadosUsuario = localStorage.getItem('dadosUsuario');
-    if(!(dadosUsuario == null || dadosUsuario == '' || dadosUsuario == undefined)){
+    if (!(dadosUsuario == null || dadosUsuario == '' || dadosUsuario == undefined)) {
         var user = dadosUsuario.split(',');
         var queryString = window.location.search;
         var id = queryString.replace(/[^0-9]/g, '');
 
-        if(id != user[0]){
+        if (id != user[0]) {
             return false;
         }
 
-        if(token == null || token == '' || token == undefined){
+        if (token == null || token == '' || token == undefined) {
             return false;
         }
 
         return true;
-    }else{
+    } else {
         return false;
     }
-    
+
 }
 
 
@@ -132,17 +133,18 @@ function urlClientePorID() {
 
 
 
-function poupularCadastro(cliente){
+function poupularCadastro(cliente) {
     var enderecos = cliente._endereco;
     console.log(enderecos)
     var enderecoEntrega = enderecos[0];
 
 
     $('#nome').val(cliente._nome);
+    $('#idDadosClienteAlterar').val(cliente._id);
     $('#email').val(cliente._email);
     $('#cpf').val(cliente._cpf);
     $('#dataNascimento').val(cliente._dataNascimento)
-    $('#senha').val(cliente._senha)
+    $('#password').val(cliente._password)
     $('#sexo').val(cliente._sexo);
     $('#celular').val(cliente._telefone);
     $('#cep').val(enderecoEntrega._cep);
@@ -155,7 +157,7 @@ function poupularCadastro(cliente){
 
 
 
-    if(enderecos.length == 2){
+    if (enderecos.length == 2) {
         var enderecoFatura = enderecos[1];
         console.log(enderecoFatura)
         $('#cepFatura').val(enderecoFatura._cep);
@@ -168,11 +170,148 @@ function poupularCadastro(cliente){
         $('enderecoFaturaDiv').show();
         var status = document.getElementById('enderecoFatura');
         status.checked = false;
-       
-    }else{
+
+    } else {
         var status = document.getElementById('enderecoFatura');
         status.checked = true;
         $('#enderecoFaturaDiv').hide();
     }
 
+}
+
+
+
+function atualizaDados() {
+    var token = localStorage.getItem('token');
+    console.log(token)
+    var data = retornarDadosUpdate();
+    var cpf = $('#cpf').val();
+    var url = 'http://localhost:8080/Clientes/atualiza?cpf=' + cpf
+
+
+
+    $.ajax({
+        url: url,
+        type: 'PUT',
+        headers: { 'TOKEN': token },
+        timeout: 2000000,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: data,
+        success: _ => {
+            var mensagem = _._message;
+            alert(mensagem);
+            window.location.reload();
+        },
+        error: result => {
+            console.log(result)
+        },
+        done: _ => {
+            alert('finalizou')
+        }
+    });
+
+}
+
+
+function retornarDadosUpdate() {
+    var nome = $('#nome').val();
+
+    var password = $('#password').val();
+
+    var sexo = $('#sexo').val();
+    var telefone = $('#celular').val();
+
+    var json = JSON.stringify({
+        _nome: nome,
+        _password: password,
+        _sexo: sexo,
+        _telefone: telefone
+
+    })
+    return json;
+}
+
+
+
+
+
+function getEnderecosLista(id) {
+
+    var url = tratarDadosgetProdutos(id);
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        timeout: 20000,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: data => {
+            var tamanho = data._endereco.length;
+            console.log('este o tamanho do array de endereço : ' + tamanho);
+            var enderecos = data._endereco;
+            console.log(tamanho);
+            //console.log(produtos);
+            for (i = 0; i < tamanho; i++) {
+                var endereco = enderecos[i];
+
+                retornarLinha(endereco);
+            }
+
+
+        },
+        error: result => {
+            alert(result.status + ' ' + result.statusText);
+        }
+    });
+}
+
+function tratarDadosgetEnderecos(id) {
+    var url1 = 'http://localhost:8080/endereco?id=' + id;
+    return url1;
+}
+
+function retornarLinha(response) {
+    console.log(response);
+
+    //instacio a tabela
+    var tabela = $('#dataTable');
+    //procuro o corpo da tabela e armazeno em uma variavel
+    var body = tabela.find('tbody');
+    //armazeno em uma variavel a linha tr = linha
+    var tr = $('<tr></tr>')
+        //cada coluna da linha eu armazeno numa var também
+    var td1 = $('<td data-rua="' + response._logradouro + '"></td>');
+    var td2 = $('<td data-numero="' + response._numero + '"></td>');
+    var td3 = $('<td data-complemento="' + response._complemento + '"></td>');
+    var td4 = $('<td data-bairro="' + response._bairro + '"></td>');
+    var td5 = $('<td data-estado="' + response._estado + '"></td>');
+
+    var td7 = $('<td data-cidade="' + response._cidade + '"></td>');
+    var td8 = $('<td data-cep="' + response._cep + '"></td>');
+
+    var td9 = $('<td onclick="mostrarModalEditar(' + response._id + ')"><a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a></td>');
+    td1.text(response._logradouro);
+    td2.text(response._numero)
+    td3.text(response._complemento);
+    td4.text(response._bairro);
+    td5.text(response._estado);
+    //td6.text(response._statusProduto);
+    td7.text(response._cidade);
+    td8.text(response._cep);
+    tr.append(td1)
+    tr.append(td2)
+    tr.append(td3)
+    tr.append(td4)
+    tr.append(td5)
+    tr.append(td6)
+    tr.append(td7)
+    tr.append(td8)
+    tr.append(td9)
+        //insiro no corpo a linha
+    body.append(tr);
+
+
+
+    return;
 }
